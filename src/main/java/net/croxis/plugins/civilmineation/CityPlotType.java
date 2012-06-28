3d23b52c6de3dc49dda6f5040f26af17239ab153
croxis <croxis@yahoo.com>
2012-06-28 13:32:15 -0700

    Possible fix for persistance

--- src/main/java/net/croxis/plugins/civilmineation/CityPlotType.java
+++ src/main/java/net/croxis/plugins/civilmineation/CityPlotType.java
@@ -1,22 +1,11 @@
 package net.croxis.plugins.civilmineation;
-
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
-
 public enum CityPlotType {
-	RESIDENTIAL(0, "default") {  // The default Block Type.
-    },
-    COMMERCIAL(1, "Shop") {  // Just like residential but has additional tax
-    },
-    ARENA(2, "arena"){	//Always PVP enabled.
-    },
-    EMBASSY(3, "embassy") {  // For other towns to own a plot in your town.
-    },
-    WILDS(4, "wilds"){	//Follows wilderness protection settings, but town owned.
-    },
-    LIBRARY(5, "library"){	//Follows wilderness protection settings, but town owned.
-    },
+	RESIDENTIAL,  // The default Block Type.
+    COMMERCIAL,  // Just like residential but has additional tax
+    ARENA,	//Always PVP enabled.
+    EMBASSY,  // For other towns to own a plot in your town.
+    WILDS,//Follows wilderness protection settings, but town owned.
+    LIBRARY,;	//Follows wilderness protection settings, but town owned.
     // These are subject to change:
 /*
     PUBLIC(5, "") {  // Will have it's own permission set
@@ -30,40 +19,4 @@ public enum CityPlotType {
 
     JAIL(8, "") {  // Where people will spawn when they die in enemy (neutral) towns
     },*/
-    ;
-    private int id;
-    private String name;
-    private static final Map<Integer,CityPlotType> idLookup
-          = new HashMap<Integer,CityPlotType>();
-    private static final Map<String,CityPlotType> nameLookup
-          = new HashMap<String,CityPlotType>();
-
-    CityPlotType(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-    
-    static {
-        for(CityPlotType s : EnumSet.allOf(CityPlotType.class)) {
-            idLookup.put(s.getId(), s);
-            nameLookup.put(s.toString().toLowerCase(), s);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public static CityPlotType lookup(int id) {
-        return idLookup.get(id);
-    }
-
-    public static CityPlotType lookup(String name) {
-        return nameLookup.get(name.toLowerCase());
-    }
 }
