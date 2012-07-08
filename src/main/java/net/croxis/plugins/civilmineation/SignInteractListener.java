873a79a47493f62a1a9b18ee35cb0999ee6bf7dd
croxis <croxis@yahoo.com>
2012-07-08 16:49:53 -0700

    Removed some debug

--- src/main/java/net/croxis/plugins/civilmineation/SignInteractListener.java
+++ src/main/java/net/croxis/plugins/civilmineation/SignInteractListener.java
@@ -19,17 +19,14 @@ public class SignInteractListener implements Listener{
 		//Left click is click, Right click is cycle
 		//Debug lines to see what the null error is from
 		//error is right clicking bottom block
-		if (event.getClickedBlock() == null){
-			Civilmineation.logDebug("z");
+		if (event.getClickedBlock() == null)
 			return;
-		}
+
 		event.getClickedBlock();
 		event.getClickedBlock().getType();
-		Civilmineation.logDebug("y");
 		if (event.getClickedBlock().getType().equals(Material.WALL_SIGN)
 				|| event.getClickedBlock().getType().equals(Material.SIGN)
 				|| event.getClickedBlock().getType().equals(Material.SIGN_POST)){
-			Civilmineation.logDebug("x");
 			if (event.getAction().equals(Action.LEFT_CLICK_BLOCK)){
 				Sign sign = (Sign) event.getClickedBlock().getState();
 				PlotComponent plot = CivAPI.getPlot(event.getClickedBlock().getChunk());
