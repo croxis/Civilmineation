230469b69d917c37b110649f64d6281615a55581
croxis <croxis@yahoo.com>
2012-07-25 18:45:34 -0700

    Sanity check

--- src/main/java/net/croxis/plugins/civilmineation/CivAPI.java
+++ src/main/java/net/croxis/plugins/civilmineation/CivAPI.java
@@ -236,144 +236,149 @@ public class CivAPI {
 		signComp = getSign(SignType.DEMOGRAPHICS, city.getEntityID());
 		if (signComp == null)
 			signComp = createSign(block.getBlock(), city.getName() + " demographics", SignType.DEMOGRAPHICS, city.getEntityID());
-		
-		if (signComp.getRotation() == 4 || signComp.getRotation() == 5){
-    		charter.getRelative(BlockFace.EAST).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.EAST).getState();
-			if (TechManager.hasTech(getMayor(city).getName(), "Currency"))
-				block.setLine(0, ChatColor.YELLOW + "Money: " + Double.toString(econ.getBalance(city.getName())));
-			else
-				block.setLine(0, ChatColor.YELLOW + "Need Currency");
-			Tech tech = TechManager.getCurrentResearch(getKing(city).getName());
-			if (tech == null){
-				block.setLine(1, "Research:");
-				block.setLine(2, "None");
-				block.setLine(3, ChatColor.BLUE + Integer.toString(TechManager.getPoints(getKing(city).getName())) + " / 0");
-			} else {
-				block.setLine(1, "Research:");
-				block.setLine(2, ChatColor.BLUE + tech.name);
-				block.setLine(3, ChatColor.BLUE + Integer.toString(TechManager.getPoints(getKing(city).getName())) + "/" + Integer.toString(TechManager.getCurrentResearch(getKing(city).getName()).cost));
+		try{
+			if (signComp.getRotation() == 4 || signComp.getRotation() == 5){
+	    		charter.getRelative(BlockFace.EAST).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.EAST).getState();
+				if (TechManager.hasTech(getMayor(city).getName(), "Currency"))
+					block.setLine(0, ChatColor.YELLOW + "Money: " + Double.toString(econ.getBalance(city.getName())));
+				else
+					block.setLine(0, ChatColor.YELLOW + "Need Currency");
+				Tech tech = TechManager.getCurrentResearch(getKing(city).getName());
+				if (tech == null){
+					block.setLine(1, "Research:");
+					block.setLine(2, "None");
+					block.setLine(3, ChatColor.BLUE + Integer.toString(TechManager.getPoints(getKing(city).getName())) + " / 0");
+				} else {
+					block.setLine(1, "Research:");
+					block.setLine(2, ChatColor.BLUE + tech.name);
+					block.setLine(3, ChatColor.BLUE + Integer.toString(TechManager.getPoints(getKing(city).getName())) + "/" + Integer.toString(TechManager.getCurrentResearch(getKing(city).getName()).cost));
+				}
+				block.update();
+				signComp = getSign(SignType.CITY_CHARTER_MONEY, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " money", SignType.CITY_CHARTER_MONEY, city.getEntityID());
+				
+				charter.getRelative(BlockFace.WEST).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.WEST).getState();
+				block.setLine(1, "Plots: " + Integer.toString(getPlots(city).size()));
+				block.setLine(2, "Culture: " + ChatColor.LIGHT_PURPLE + Integer.toString(city.getCulture()));
+				block.update();
+				signComp = getSign(SignType.CITY_CHARTER_CULTURE, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " culture", SignType.CITY_CHARTER_CULTURE, city.getEntityID());
+				
+				charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getState();
+				block.setLine(0, "Civilization");
+				block.setLine(1, "Build/Destroy");
+				if (getPermissions(city.getEntityID()).allyEdit)
+					block.setLine(3, ChatColor.GREEN + "Open");
+				else
+					block.setLine(3, ChatColor.RED + "Closed");
+				block.update();
+				signComp = getSign(SignType.CITY_PERM_CIV_BUILD, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " perm civ build", SignType.CITY_PERM_CIV_BUILD, city.getEntityID());
+				
+				charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getRelative(BlockFace.UP).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getRelative(BlockFace.UP).getState();
+				block.setLine(0, "Resident");
+				block.setLine(1, "Build/Destroy");
+				if (getPermissions(city.getEntityID()).residentEdit)
+					block.setLine(3, ChatColor.GREEN + "Open");
+				else
+					block.setLine(3, ChatColor.RED + "Closed");
+				block.update();
+				signComp = getSign(SignType.CITY_PERM_RES_BUILD, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " perm res build", SignType.CITY_PERM_RES_BUILD, city.getEntityID());
+				
+				charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getRelative(BlockFace.DOWN).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getRelative(BlockFace.DOWN).getState();
+				block.setLine(0, "Outsider");
+				block.setLine(1, "Build/Destroy");
+				if (getPermissions(city.getEntityID()).outsiderEdit)
+					block.setLine(3, ChatColor.GREEN + "Open");
+				else
+					block.setLine(3, ChatColor.RED + "Closed");
+				block.update();
+				signComp = getSign(SignType.CITY_PERM_OUT_BUILD, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " perm out build", SignType.CITY_PERM_OUT_BUILD, city.getEntityID());
+				
+			} else if (signComp.getRotation() == 2 || signComp.getRotation() == 3) {
+				charter.getRelative(BlockFace.NORTH).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.NORTH).getState();
+				block.setLine(0, "Money: N/A");
+				Tech tech = TechManager.getCurrentResearch(getKing(city).getName());
+				if (tech == null){
+					block.setLine(1, "Research:");
+					block.setLine(2, "None");
+					block.setLine(3, ChatColor.BLUE + Integer.toString(TechManager.getPoints(getKing(city).getName())) + " / 0");
+				} else {
+					block.setLine(1, "Research:");
+					block.setLine(2, ChatColor.BLUE + tech.name);
+					block.setLine(3, ChatColor.BLUE + Integer.toString(TechManager.getPoints(getKing(city).getName())) + "/" + Integer.toString(TechManager.getCurrentResearch(getKing(city).getName()).cost));
+				}
+				block.update();
+				signComp = getSign(SignType.CITY_CHARTER_MONEY, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " demographics", SignType.CITY_CHARTER_MONEY, city.getEntityID());
+				
+				charter.getRelative(BlockFace.SOUTH).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.SOUTH).getState();
+				block.setLine(1, "Plots: N/A");
+				block.setLine(2, "Culture: " + ChatColor.LIGHT_PURPLE + Integer.toString(city.getCulture()));
+				block.update();
+				signComp = getSign(SignType.CITY_CHARTER_CULTURE, city.getEntityID());
+				if (signComp == null)
+					createSign(block.getBlock(), city.getName() + " demographics", SignType.CITY_CHARTER_CULTURE, city.getEntityID());
+				
+				charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getState();
+				block.setLine(0, "Civilization");
+				block.setLine(1, "Build/Destroy");
+				if (getPermissions(city.getEntityID()).allyEdit)
+					block.setLine(3, ChatColor.GREEN + "Open");
+				else
+					block.setLine(3, ChatColor.RED + "Closed");
+				block.update();
+				signComp = getSign(SignType.CITY_PERM_CIV_BUILD, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " perm civ build", SignType.CITY_PERM_CIV_BUILD, city.getEntityID());
+				
+				charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getRelative(BlockFace.UP).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getRelative(BlockFace.UP).getState();
+				block.setLine(0, "Resident");
+				block.setLine(1, "Build/Destroy");
+				if (getPermissions(city.getEntityID()).residentEdit)
+					block.setLine(3, ChatColor.GREEN + "Open");
+				else
+					block.setLine(3, ChatColor.RED + "Closed");
+				block.update();
+				signComp = getSign(SignType.CITY_PERM_RES_BUILD, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " perm res build", SignType.CITY_PERM_RES_BUILD, city.getEntityID());
+				
+				charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getRelative(BlockFace.DOWN).setTypeIdAndData(68, signComp.getRotation(), true);
+				block = (Sign) charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getRelative(BlockFace.DOWN).getState();
+				block.setLine(0, "Outsider");
+				block.setLine(1, "Build/Destroy");
+				if (getPermissions(city.getEntityID()).outsiderEdit)
+					block.setLine(3, ChatColor.GREEN + "Open");
+				else
+					block.setLine(3, ChatColor.RED + "Closed");
+				block.update();
+				signComp = getSign(SignType.CITY_PERM_OUT_BUILD, city.getEntityID());
+				if (signComp == null)
+					signComp = createSign(block.getBlock(), city.getName() + " perm out build", SignType.CITY_PERM_OUT_BUILD, city.getEntityID());
+				
 			}
-			block.update();
-			signComp = getSign(SignType.CITY_CHARTER_MONEY, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " money", SignType.CITY_CHARTER_MONEY, city.getEntityID());
-			
-			charter.getRelative(BlockFace.WEST).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.WEST).getState();
-			block.setLine(1, "Plots: " + Integer.toString(getPlots(city).size()));
-			block.setLine(2, "Culture: " + ChatColor.LIGHT_PURPLE + Integer.toString(city.getCulture()));
-			block.update();
-			signComp = getSign(SignType.CITY_CHARTER_CULTURE, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " culture", SignType.CITY_CHARTER_CULTURE, city.getEntityID());
-			
-			charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getState();
-			block.setLine(0, "Civilization");
-			block.setLine(1, "Build/Destroy");
-			if (getPermissions(city.getEntityID()).allyEdit)
-				block.setLine(3, ChatColor.GREEN + "Open");
-			else
-				block.setLine(3, ChatColor.RED + "Closed");
-			block.update();
-			signComp = getSign(SignType.CITY_PERM_CIV_BUILD, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " perm civ build", SignType.CITY_PERM_CIV_BUILD, city.getEntityID());
-			
-			charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getRelative(BlockFace.UP).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getRelative(BlockFace.UP).getState();
-			block.setLine(0, "Resident");
-			block.setLine(1, "Build/Destroy");
-			if (getPermissions(city.getEntityID()).residentEdit)
-				block.setLine(3, ChatColor.GREEN + "Open");
-			else
-				block.setLine(3, ChatColor.RED + "Closed");
-			block.update();
-			signComp = getSign(SignType.CITY_PERM_RES_BUILD, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " perm res build", SignType.CITY_PERM_RES_BUILD, city.getEntityID());
-			
-			charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getRelative(BlockFace.DOWN).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.EAST).getRelative(BlockFace.EAST).getRelative(BlockFace.DOWN).getState();
-			block.setLine(0, "Outsider");
-			block.setLine(1, "Build/Destroy");
-			if (getPermissions(city.getEntityID()).outsiderEdit)
-				block.setLine(3, ChatColor.GREEN + "Open");
-			else
-				block.setLine(3, ChatColor.RED + "Closed");
-			block.update();
-			signComp = getSign(SignType.CITY_PERM_OUT_BUILD, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " perm out build", SignType.CITY_PERM_OUT_BUILD, city.getEntityID());
-			
-		} else if (signComp.getRotation() == 2 || signComp.getRotation() == 3) {
-			charter.getRelative(BlockFace.NORTH).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.NORTH).getState();
-			block.setLine(0, "Money: N/A");
-			Tech tech = TechManager.getCurrentResearch(getKing(city).getName());
-			if (tech == null){
-				block.setLine(1, "Research:");
-				block.setLine(2, "None");
-				block.setLine(3, ChatColor.BLUE + Integer.toString(TechManager.getPoints(getKing(city).getName())) + " / 0");
-			} else {
-				block.setLine(1, "Research:");
-				block.setLine(2, ChatColor.BLUE + tech.name);
-				block.setLine(3, ChatColor.BLUE + Integer.toString(TechManager.getPoints(getKing(city).getName())) + "/" + Integer.toString(TechManager.getCurrentResearch(getKing(city).getName()).cost));
-			}
-			block.update();
-			signComp = getSign(SignType.CITY_CHARTER_MONEY, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " demographics", SignType.CITY_CHARTER_MONEY, city.getEntityID());
-			
-			charter.getRelative(BlockFace.SOUTH).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.SOUTH).getState();
-			block.setLine(1, "Plots: N/A");
-			block.setLine(2, "Culture: " + ChatColor.LIGHT_PURPLE + Integer.toString(city.getCulture()));
-			block.update();
-			signComp = getSign(SignType.CITY_CHARTER_CULTURE, city.getEntityID());
-			if (signComp == null)
-				createSign(block.getBlock(), city.getName() + " demographics", SignType.CITY_CHARTER_CULTURE, city.getEntityID());
-			
-			charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getState();
-			block.setLine(0, "Civilization");
-			block.setLine(1, "Build/Destroy");
-			if (getPermissions(city.getEntityID()).allyEdit)
-				block.setLine(3, ChatColor.GREEN + "Open");
-			else
-				block.setLine(3, ChatColor.RED + "Closed");
-			block.update();
-			signComp = getSign(SignType.CITY_PERM_CIV_BUILD, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " perm civ build", SignType.CITY_PERM_CIV_BUILD, city.getEntityID());
-			
-			charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getRelative(BlockFace.UP).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getRelative(BlockFace.UP).getState();
-			block.setLine(0, "Resident");
-			block.setLine(1, "Build/Destroy");
-			if (getPermissions(city.getEntityID()).residentEdit)
-				block.setLine(3, ChatColor.GREEN + "Open");
-			else
-				block.setLine(3, ChatColor.RED + "Closed");
-			block.update();
-			signComp = getSign(SignType.CITY_PERM_RES_BUILD, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " perm res build", SignType.CITY_PERM_RES_BUILD, city.getEntityID());
-			
-			charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getRelative(BlockFace.DOWN).setTypeIdAndData(68, signComp.getRotation(), true);
-			block = (Sign) charter.getRelative(BlockFace.NORTH).getRelative(BlockFace.NORTH).getRelative(BlockFace.DOWN).getState();
-			block.setLine(0, "Outsider");
-			block.setLine(1, "Build/Destroy");
-			if (getPermissions(city.getEntityID()).outsiderEdit)
-				block.setLine(3, ChatColor.GREEN + "Open");
-			else
-				block.setLine(3, ChatColor.RED + "Closed");
-			block.update();
-			signComp = getSign(SignType.CITY_PERM_OUT_BUILD, city.getEntityID());
-			if (signComp == null)
-				signComp = createSign(block.getBlock(), city.getName() + " perm out build", SignType.CITY_PERM_OUT_BUILD, city.getEntityID());
-			
+		} catch (Exception e){
+			broadcastToCity("WARNING. CITY CHARTER IS MALFORMED. PLEASE FIX", city);
+			Civilmineation.log("WARNING CITY CHARTER MALFORMED: " + city.getName());
+			e.printStackTrace();
 		}
     }
     
